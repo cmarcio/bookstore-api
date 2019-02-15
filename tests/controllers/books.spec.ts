f2be7288a878c1dc8e14e52e19234ef5d98923eb
Marcio Campos <marciodscampos@gmail.com>
2019-02-15 11:33:49 -0200

    Created tests for books controller

--- tests/controllers/books.spec.ts
+++ tests/controllers/books.spec.ts
@@ -1,5 +1,31 @@
+// tslint:disable-next-line:import-name
+import request from 'supertest';
+import app from '../../src/app';
+
 describe('Books Costroller', () => {
-  describe('POST /book', () => {});
-  describe('GET /books', () => {});
-  describe('GET /book/id', () => {});
+  describe('POST /book', () => {
+    it('should respond with 200', async () => {
+      await request(app)
+        .post('/book')
+        .expect(200);
+    });
+  });
+
+  describe('GET /books', () => {
+    it('should respond with 200', async () => {
+      await request(app)
+        .get('/books')
+        .expect(200);
+    })
+  });
+
+  describe('GET /book/id', () => {
+    const id = 23;
+
+    it('should respond with 200', async () => {
+      await request(app)
+        .get(`/books/${id}`)
+        .expect(200);
+    });
+  });
 });
